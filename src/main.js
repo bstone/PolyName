f55f185cb4464e572a36ff13387d37106cfd4704
Branden Stone <bstone@adelphi.edu>
2016-07-05 16:01:42 -0400

    added comments

--- src/main.js
+++ src/main.js
@@ -84,7 +84,7 @@ d3.select('#make-pdf')
     doc.setFontSize(40);
     doc.text(35, 25, "Personal Polynomial!");
     doc.addImage(dataURL, 'png', 15, 40, 180, 90);
-    doc.fromHTML(usernameToPrint + " = ",20, 150, {
+    doc.fromHTML(usernameToPrint + " = ",20, 150, { // this may not need the from HTML, it is just a string.
       'width': 170, 
       });    
     doc.fromHTML(pdfPolynomial,25, 155, {
