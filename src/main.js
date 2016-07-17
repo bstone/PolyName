d6b7ae083501a37851452449d71249b742dd6263
Branden Stone <bstone@adelphi.edu>
2016-07-17 15:15:12 -0400

    made the pdf look a bit nicer, still needs help

--- src/main.js
+++ src/main.js
@@ -573,41 +573,6 @@ function makePoly(points) {
   return polynomial;
 }
 
-// The table generation function
-function tabulate(data, columns) {
-    var table = d3.select("#nam-table").append("table")
-            .attr("style", "margin-left: 250px"),
-        thead = table.append("thead"),
-        tbody = table.append("tbody");
-
-    // append the header row
-    thead.append("tr")
-        .selectAll("th")
-        .data(columns)
-        .enter()
-        .append("th")
-            .text(function(column) { return column; });
-
-    // create a row for each object in the data
-    var rows = tbody.selectAll("tr")
-        .data(data)
-        .enter()
-        .append("tr");
-
-    // create a cell in each row for each column
-    var cells = rows.selectAll("td")
-        .data(function(row) {
-            return columns.map(function(column) {
-                return {column: column, value: row[column]};
-            });
-        })
-        .enter()
-        .append("td")
-        .attr("style", "font-family: Courier") // sets the font style
-            .html(function(d) { return d.value; });
-    console.log("hello it works");
-    return table;
-}
 
 
 
--- src/makepdf.js
+++ src/makepdf.js
@@ -4,15 +4,29 @@ d3.select('#make-pdf')
 
     var doc = new jsPDF();
 
+    doc.setDrawColor(255,0,0);
+    doc.rect(5, 5, 200, 287 );
+
     doc.setFontSize(40);
     doc.text(35, 25, "Personal Polynomial!");
+    doc.setFontSize(14);    
     doc.addImage(dataURL, 'png', 15, 40, 180, 90);
-    doc.fromHTML(usernameToPrint + " = ",20, 150, { // this may not need the from HTML, it is just a string.
+    doc.fromHTML(usernameToPrint + " = ",20, 140, { // this may not need the from HTML, it is just a string.
       'width': 170, 
       });    
-    doc.fromHTML(pdfPolynomial,25, 155, {
-      'width': 165, 
+    doc.fromHTML(pdfPolynomial,25, 145, {
+      'width': 130, 
       });
+    doc.fromHTML(d3.select('#name-table').html(),165, 140, {
+      'width': 30, 
+      });    
+
+    doc.text(25, 200, "Like you, the above polynomial is the only one of its kind!");    
+    doc.text(25, 205, "It is the only polynomial of its degree that passes through");    
+    doc.text(25,210, "the points on the right.");    
+
+
+
     var filename = usernameUC.join("");
     doc.save(filename+'.pdf');
   });
